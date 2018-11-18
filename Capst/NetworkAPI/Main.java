c1189e9a7043760746c7e82d3587afb178ef1ebe
OhEun Kwon <dhdms6512@gmail.com>
2018-11-18 21:47:42 +0900

    Add files via upload
    Upload Capstone files

---
+++ Capst/NetworkAPI/Main.java
@@ -0,0 +1,11 @@
+package NetworkAPI;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        NetworkAPI test = new NetworkAPI();
+        test.GetLocalAddr();
+
+    }
+}
